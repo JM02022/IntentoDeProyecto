ad6e4727062f1f7a67e42054572cc2d00137864c
JM02022 <018101636j@uandina.edu.pe>
2022-08-23 17:33:59 -0500

    actulizacion

--- js/funciones.js
+++ js/funciones.js
@@ -1,3 +1,22 @@
+var ventanas = []
+function aniadirDiv(text){
+  var newDiv = document.createElement("div")
+  newDiv.textContent = "• " + text
+  const box = document.getElementById("lista")
+  box.appendChild(newDiv)
+}
+function cerrarPagina(text,pos){
+  window.close()
+}
+function speak(text){
+  var msg = new SpeechSynthesisUtterance(text)
+  var voices = window.speechSynthesis.getVoices() 
+  msg.voice = voices[8]
+  window.speechSynthesis.speak(msg)
+}
+function buscarListaMusica(resultado){
+  console.log(resultado.results[0][0].transcript.toLowerCase().trim())
+}
 function run() {
   const recognition = new (window.SpeechRecognition ||
     window.webkitSpeechRecognition ||
@@ -22,33 +41,45 @@ function run() {
     //document.body.innerHTML = resultado.results[0][0].transcript;
     /* Adicional */
     if (
-      resultado.results[0][0].transcript.toLowerCase().trim() ==
-      "abrir wikipedia"
-    ) {
-      const childFrame = document.createElement("iframe");
-      childFrame.src = "https://es.wikipedia.org/wiki/Wikipedia:Portada";
-      childFrame.style.width = "100vw";
-      childFrame.style.height = "500px";
-      document.body.append(childFrame);
-    } else if (
-      resultado.results[0][0].transcript.toLowerCase().trim() == "música"
+      resultado.results[0][0].transcript.toLowerCase().trim() == "salsa"
     ) {
       window.open(
-        "https://www.youtube.com/watch?v=z6GgrL6a-cc&ab_channel=JulianRosero",
+        "https://music.youtube.com/watch?v=Ty2wCuHfNN8&list=RDCLAK5uy_lc4yBYpywmmgjplkuzYxzUgLJ8UvDKUUk",
         "_blank"
       );
-    } else if (
+      aniadirDiv("reproduciendo musica - salsa")
+      ventanas.push("https://www.youtube.com/watch?v=z6GgrL6a-cc&ab_channel=JulianRosero")
+
+    }
+    else if (
+      resultado.results[0][0].transcript.toLowerCase().trim() == "pon esa mierda de música"
+    ) {
+      window.open(
+        "https://music.youtube.com/watch?v=cUmSm7-_YhM&list=RDAMVMcUmSm7-_YhM",
+        "_blank"
+      );
+      aniadirDiv("reproduciendo musica - musica de mierda")
+      ventanas.push("https://music.youtube.com/watch?v=cUmSm7-_YhM&list=RDAMVMcUmSm7-_YhM")
+
+    } 
+    else if (
       resultado.results[0][0].transcript.toLowerCase().trim() == "whatsapp"
     ) {
       window.open("https://web.whatsapp.com/", "_blank");
+      aniadirDiv("whatsapp.com")
+      ventanas.push("https://web.whatsapp.com/")
+      speak("abriendo whatsapp")
     } else if (
-      resultado.results[0][0].transcript.toLowerCase().trim() == "stop"
+      resultado.results[0][0].transcript.toLowerCase().trim() == "cerrar" 
     ) {
-      recognition.aborted();
+      speak("¿que pestaña deseas cerrar?")
     } else if (
       resultado.results[0][0].transcript.toLowerCase().trim() == "aula virtual"
     ) {
       window.open("https://campus.uandina.edu.pe/login/index.php", "_blank");
+      aniadirDiv("campus.uandina.edu.pe")
+      ventanas.push("https://campus.uandina.edu.pe/login/index.php")
+      speak("abriendo aula virtual")
     } else {
       var url =
         "https://" +
@@ -60,6 +91,3 @@ function run() {
     }
   };
 }
-function stop() {}
-
-// comentar ctrl+k+c  descomentar ctrl+k+u
